db506db3a04c3bc1e05b1f8b45a7cdc0882b2b08
Carlos Alvarado <xavieralvaradom@gmail.com>
2021-06-26 17:28:43 -0400

    contenido formulario

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,21 @@
+class PostsController < ApplicationController
+    def index
+        @posts = Post.all
+    end
+
+    def new
+        @post = Post.new
+    end
+    
+    def create
+        post = Post.new(post_params)
+            post.content = post.content.gsub! 'spoiler', ''
+        post.save
+
+        redirect_to root_path
+    end
+
+    def post_params
+        params.require(:post).permit(:tittle, :image_url, :content)
+    end
+end
